e22bbb0a75ee778d90f3df46b4fd97460cea5306
Redha Azmei <redha.azmei@gmail.com>
2023-07-03 02:33:21 +0800

    Create Billplz BaseAPI class

---
+++ src/resources/base.ts
@@ -0,0 +1,58 @@
+import fetch, { Response, RequestInit } from 'node-fetch';
+
+export interface APIConfig {
+  /**
+   * The API key for your Billplz account.
+   */
+  apiKey: string;
+
+  /**
+   * Specify environment to be used.
+   * Options either `sandbox` or `production` environment.
+   */
+  environment: 'sandbox' | 'production';
+}
+
+export class BaseAPI {
+  protected apiKey: string;
+  protected apiUrl: string;
+
+  constructor(config: APIConfig) {
+    this.apiKey = Buffer.from(config.apiKey).toString('base64');
+    this.apiUrl =
+      config.environment === 'sandbox'
+        ? 'https://www.billplz-sandbox.com/api/v3/'
+        : 'https://www.billplz.com/api/v3/';
+  }
+
+  protected createURLSearchParams(body: Record<string, any>): URLSearchParams {
+    const params = new URLSearchParams();
+    function traverseObject(body: Record<string, any>, prefix = ''): void {
+      for (const [key, value] of Object.entries(body)) {
+        const paramKey = prefix ? `${prefix}[${key}]` : key;
+        if (typeof value === 'object' && !Array.isArray(value)) {
+          traverseObject(value, paramKey);
+        } else {
+          params.append(paramKey, String(value));
+        }
+      }
+    }
+    traverseObject(body);
+    return params;
+  }
+
+  protected async request(method: string, path: string, body: object): Promise<Response> {
+    const url = `${this.apiUrl}${path}`;
+    const headers = {
+      Authorization: `Basic ${this.apiKey}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    const options: RequestInit = {
+      method,
+      headers,
+      body: this.createURLSearchParams(body),
+    };
+    const response: Response = await fetch(url, options);
+    return response;
+  }
+}
